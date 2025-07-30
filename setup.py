bbbec5a8a969f6f4a98e1d8faf1c2c0f6eae5345
Chandan <chandankumarswain2468@gmail.com>
2025-07-30 11:26:46 +0530

    Structure-Ready

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="Rossmann store sales price prediction",
+    author="chandan",
+    author_email="chandankumarswain3467@gmail.com",
+    packages=find_packages(),
+)
---
+++ src/constants/__init__.py
@@ -0,0 +1,4 @@
+# MonogDB constants
+DATABASE_NAME = "Rossmann-data"
+COLLECTION_NAME = "Rossmann-collection"
+CONNECTION_STRING = "MONGODB_URL"
